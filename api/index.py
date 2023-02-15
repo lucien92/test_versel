2c2a425ecafca5ee226fc240381561289346afdb
lucien92 <lucien.rondier@student-cs.fr>
2023-02-15 23:19:12 +0100

    update

--- api/index.py
+++ api/index.py
@@ -1,26 +1,23 @@
 from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+from flask import url_for
 
 app = Flask(__name__)
 
 @app.route('/', methods=["GET", "POST"])
 def survey():
-    # if request.method == 'POST':
-    #     result = request.form['number'] #permet de récupérer le résultat du formulaire
-    #     return redirect(url_for('result', variable=result)) #rédirige vers une autre page en faisant passer une variable
+    if request.method == 'POST':
+        result = request.form['number'] #permet de récupérer le résultat du formulaire
+        return redirect(url_for('result', variable=result)) #rédirige vers une autre page en faisant passer une variable
 
     # don't need to test request.method == 'GET'
     return render_template('form.html')
 
-# @app.route('/')
-# def hello():
-#     return 'Hello, world'
 
 
-@app.route('/test')
-def test():
-    return 'Test'
-
-@app.route('/result')
-def result():
-   dict = {'phy':50,'che':60,'maths':70}
-   return render_template('result.html', result = dict)
+@app.route('/result/<variable>') #attention il faut bien mettre à la fin du chemin <variabnle> pour que la fonction result puisse récupérer le nom de la variable mis dans le redirect
+def result(variable): #opn reprend le nom de la variable qui a été définie dans la fonction survey
+     contenance = variable
+     #result_final = algo(contenance) #OK ça devrait être bon, à tester en attendant et en améliorant le rendu de la page
+     
+     return render_template('result.html', result=contenance) #voir comment utiliser une variable dans le result.html
