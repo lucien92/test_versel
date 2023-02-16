4482ce1cfa8d6c5fe32300ffd94051e3384fa3d7
lucien92 <lucien.rondier@student-cs.fr>
2023-02-16 17:11:11 +0100

    without pandas

--- api/index.py
+++ api/index.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 from flask import Flask, render_template, request, redirect
 from flask import url_for
-from algo import algo
+#from algo import algo
 
 app = Flask(__name__)
 
